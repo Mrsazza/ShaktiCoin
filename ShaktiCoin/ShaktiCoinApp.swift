0b9709c0252173a71dd368839d0f9f9d34389b30
Mrsazza <shahriarhossain147@gmail.com>
2022-04-29 17:50:55 +0600

    Initial Commit

---
+++ ShaktiCoin/ShaktiCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaktiCoinApp.swift
+//  ShaktiCoin
+//
+//  Created by Sazza on 29/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ShaktiCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
